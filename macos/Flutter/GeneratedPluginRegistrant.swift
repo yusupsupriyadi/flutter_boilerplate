0f035edda9e5778c7859182e60800f0ad12d68a8
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2026-07-05 10:13:52 +0700

    chore(platform): network config and generated launcher icons + native splash

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import flutter_secure_storage_darwin
+import shared_preferences_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
